52503b4e79451f183f2da236a1bf94de4cf3dbf8
Robert-Aimes <65087616+Robert-Aimes@users.noreply.github.com>
2024-01-23 14:08:41 -0600

    Update ToDoList.java

--- ToDoList.java
+++ ToDoList.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToDoList {
+    private List<ToDo> toDoList;
+
+    public ToDoList(){
+        toDoList = new ArrayList<>();
+
+    }
+
+    public void addToDo(String item, String dueDate){
+        ToDo newItem = new ToDo(item, dueDate);
+        toDoList.add(newItem);
+    }
 }
