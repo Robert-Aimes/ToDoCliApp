b77277e81f8f93fda87dab191b4bb8dff083e5f6
Robert-Aimes <65087616+Robert-Aimes@users.noreply.github.com>
2024-01-23 13:33:22 -0600

    Create ToDo.java

---
+++ ToDo.java
@@ -0,0 +1,19 @@
+public class ToDo {
+    private String item;
+    private String dueDate;
+    public ToDo(String item, String dueDate){
+        this.item = item;
+        this.dueDate = dueDate;
+    }
+
+    public String getItem(){
+        return item;
+    }
+
+    public String getDueDate(){
+        return dueDate;
+    }
+
+
+
+}
